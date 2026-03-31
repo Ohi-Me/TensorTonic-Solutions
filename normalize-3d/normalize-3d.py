d59cba1988f6487fc07f35c4da7875dbfdc2e5e4
Rohit Kumar <120547475+Ohi-Me@users.noreply.github.com>
2026-03-31 13:53:49 +0530

    Update solution for normalize-3d

--- normalize-3d/normalize-3d.py
+++ normalize-3d/normalize-3d.py
@@ -1,40 +1,34 @@
 import numpy as np
-from math import pow
 
 def normalize_3d(v):
-    v = np.asarray(v, dtype=float)
-    
-    # case 1: single vector (3,)
-    if v.ndim == 1:
-        n = len(v)
-        mag = 0
-        for i in range(n):
-            mag += pow(v[i], 2)
-        mag = pow(mag, 0.5)
-        
-        if mag == 0:
+    """
+    Normalize 3D vector(s) to unit length.
+    """
+    # Your code here
+    v = np.asarray(v,dtype=float)
+    n=len(v)
+    mag=0
+
+    if(v.ndim==1):
+        for i in range(3):
+            mag=mag + pow(v[i],2)
+        if(mag==0):
             return v
-        
-        for i in range(n):
-            v[i] = v[i] / mag
-        
-        return v
-    
-    # case 2: batch (N,3)
+        else:
+            mag=pow(mag,0.5)
+            for i in range(3):
+                v[i]= v[i]/mag
+            return v
     else:
-        n = len(v)
-        res = np.zeros_like(v)
-        
         for i in range(n):
-            mag = 0
+            mag=0
             for j in range(3):
-                mag += pow(v[i][j], 2)
-            mag = pow(mag, 0.5)
-            
-            if mag == 0:
-                res[i] = v[i]
+                mag=mag + pow(v[i][j],2)
+            if(mag==0):
+                v[i]=v[i]
             else:
-                for j in range(3):
-                    res[i][j] = v[i][j] / mag
-        
-        return res
+                mag= pow(mag,0.5)
+                for k in range(3):
+                    v[i][k]=v[i][k] / mag
+    return v
+    pass
