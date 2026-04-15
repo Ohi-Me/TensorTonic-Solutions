a2567a618e51958edf4dba864ccd6a126c600452
Rohit Kumar <120547475+Ohi-Me@users.noreply.github.com>
2026-04-15 21:05:49 +0530

    Add solution for jaccard-similarity

---
+++ jaccard-similarity/jaccard-similarity.py
@@ -0,0 +1,16 @@
+def jaccard_similarity(set_a, set_b):
+    """
+    Compute the Jaccard similarity between two item sets.
+    """
+    # Write code here
+    a = list(set(set_a))
+    b = list(set(set_b))
+    both=0
+    n=len(a)
+    m=len(b)
+    for i in range(n):
+        if a[i] in b:
+            both+=1
+    notcommon=n+m-both
+    # return both/notcommon
+    return both / notcommon if notcommon != 0 else 0
